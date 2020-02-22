a9b2c61e02a8622838f1d0be72ba87ee0259d3de
Kim HyunWoo <stories2@naver.com>
2020-02-22 18:17:19 +0900

    해상도 변경 대응

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ export default {
       pixiApp: null,
     }
   },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
+  },
   beforeCreate: function() {
      let type = 'WebGL';
      if (!utils.isWebGLSupported()) {
@@ -20,19 +23,35 @@ export default {
      utils.sayHello(type);
   },
   created: function() {
+    console.log('event reg')
+    window.addEventListener('resize', this.handleResize)
   },
   mounted: function() {
     const options = {
       width: screen.availWidth,
       height: screen.availHeight,
       transparent: false,
-      antialias: true
+      antialias: true,
+      resolution: 1
     };
     console.log('pixi options', options);
     this.pixiApp = new Application(options)
     console.log('pixi app', this.pixiApp)
     
     this.$el.appendChild(this.pixiApp.view)
+
+    this.pixiApp.renderer.backgroundColor = 0x061639;
+    this.pixiApp.renderer.autoResize = true;
+    this.pixiApp.renderer.view.style.position = "absolute";
+    this.pixiApp.renderer.view.style.display = "block";
+    this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+    // this.pixiApp.renderer.resize(512, 512);
+  },
+  methods: {
+    handleResize: function($event) {
+      console.log(`set pixi app size ${window.innerWidth} x ${window.innerHeight}`);
+      this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+    }
   },
   components: {
 
