89c6510b08c0b3e8a4113c33061f5af83051d2ee
Kim HyunWoo <stories2@naver.com>
2020-02-22 18:06:51 +0900

    pixijs canvas 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,47 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div>
   </div>
 </template>
 
 <script>
-import { utils } from 'pixi.js';
+import { utils, Application } from 'pixi.js';
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      pixiApp: null,
     }
   },
-  created: function() {
+  beforeCreate: function() {
      let type = 'WebGL';
      if (!utils.isWebGLSupported()) {
        type = 'canvas'
      }
      utils.sayHello(type);
+  },
+  created: function() {
+  },
+  mounted: function() {
+    const options = {
+      width: screen.availWidth,
+      height: screen.availHeight,
+      transparent: false,
+      antialias: true
+    };
+    console.log('pixi options', options);
+    this.pixiApp = new Application(options)
+    console.log('pixi app', this.pixiApp)
+    
+    this.$el.appendChild(this.pixiApp.view)
+  },
+  components: {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  div {
+    height: 100%;
+  }
 </style>
