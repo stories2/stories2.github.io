5b33aba37e433347af1732fdd45ee9864e01da49
Kim HyunWoo <stories2@naver.com>
2020-02-22 19:12:40 +0900

    이미지 로드 후 렌더링

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
 </template>
 
 <script>
-import { utils, Application } from 'pixi.js';
+import { utils, Application, Sprite, stage} from 'pixi.js';
 export default {
   name: 'app',
   data () {
     return {
       pixiApp: null,
+      digChanSprite: null,
+      ratio: 1
     }
   },
   beforeDestroy: function () {
@@ -34,6 +36,9 @@ export default {
       antialias: true,
       resolution: 1
     };
+
+    this.ratio = screen.availWidth / screen.availHeight
+
     console.log('pixi options', options);
     this.pixiApp = new Application(options)
     console.log('pixi app', this.pixiApp)
@@ -46,11 +51,35 @@ export default {
     this.pixiApp.renderer.view.style.display = "block";
     this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
     // this.pixiApp.renderer.resize(512, 512);
+
+    this.resourceLoader();
   },
   methods: {
     handleResize: function($event) {
       console.log(`set pixi app size ${window.innerWidth} x ${window.innerHeight}`);
       this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+
+      this.ratio = screen.innerWidth / screen.innerHeight
+    },
+    resourceLoader: function() {
+      // const texture = utils.TextureCache[require('assets/dig-chan.png')]
+      this.digChanSprite = Sprite.from('dist/assets/dig-chan.png');
+
+      console.log('sprite', this.digChanSprite)
+
+      this.pixiApp.stage.addChild(this.digChanSprite);
+
+      const digChanRatio = this.digChanSprite.width / this.digChanSprite.height;
+      this.digChanSprite.width = 400;
+      this.digChanSprite.height = 400 * digChanRatio;
+
+      this.digChanSprite.anchor.x = 0.5
+      this.digChanSprite.anchor.y = 0.5
+
+      this.digChanSprite.x = screen.width / 2;
+      this.digChanSprite.y = screen.height / 2
+
+      this.digChanSprite.rotation = -0.25;
     }
   },
   components: {
